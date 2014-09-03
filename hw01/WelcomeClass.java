bfbe6b4468a7a8aa2aca6e592164e33d97b94e63
jseto3 <jys217@lehigh.edu>
2014-09-03 00:01:10 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,27 @@
+///////////////////////////////////////////////////////////////////////////////
+//Jeffrey Seto
+//September 2, 2014
+//CSE 002
+//Welcome Class Java Program
+//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+  public static void main(String[] args) {
+      
+//  print the statement with box around it
+    System.out.println("  -----------");
+    System.out.println("  | WELCOME |");
+    System.out.println("  -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^  ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("|-J--Y--S--2--1--7-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v  ");
+    System.out.println("In my free time, I enjoy mountain biking and snowboarding.");
+    
+    //test/testtest
+  } 
+}
